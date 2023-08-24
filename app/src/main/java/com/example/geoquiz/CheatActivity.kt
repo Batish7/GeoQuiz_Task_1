4814390c40b1073a8b92efb7da5fcfb7dd67c929
Batish <S_y048@student.usc.edu.au>
2023-08-24 13:36:44 +1000

    Initial commit

---
+++ app/src/main/java/com/example/geoquiz/CheatActivity.kt
@@ -0,0 +1,67 @@
+package com.example.geoquiz
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+const val EXTRA_ANSWER_SHOWN = "com.example.geoquiz.answer_shown"
+private const val EXTRA_ANSWER_IS_TRUE = "com.example.geoquiz.answer_is_true"
+private const val KEY_WAS_CHEATED = "was_cheated"
+
+class CheatActivity : AppCompatActivity() {
+    private lateinit var answerTextView: TextView
+    private lateinit var showAnswerButton: Button
+    private var answerIsTrue = false
+    private var wasCheated = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cheat)
+
+        wasCheated = savedInstanceState?.getBoolean(KEY_WAS_CHEATED, false) ?: false
+
+        answerIsTrue = intent.getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false)
+
+        answerTextView = findViewById(R.id.answer_text_view)
+        showAnswerButton = findViewById(R.id.show_answer_button)
+
+        showAnswerButton.setOnClickListener {
+            wasCheated = true
+            setAnswerShownResult(true)
+            fillTextIfCheated()
+        }
+
+        fillTextIfCheated()
+    }
+
+    private fun fillTextIfCheated() {
+        if (wasCheated) {
+            val answerText = getString(if (answerIsTrue) R.string.true_button else R.string.false_button)
+            answerTextView.text = answerText
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean(KEY_WAS_CHEATED, wasCheated)
+    }
+
+    private fun setAnswerShownResult(isAnswerShown: Boolean) {
+        val data = Intent().apply {
+            putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown)
+        }
+        setResult(Activity.RESULT_OK, data)
+    }
+
+    companion object {
+        fun newIntent(packageContext: Context, answerIsTrue: Boolean): Intent {
+            return Intent(packageContext, CheatActivity::class.java).apply {
+                putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -0,0 +1,93 @@
+/*
+* Student Name - Batish Yadav
+* Student Id - 1148502
+* Task1- CSC202
+* */
+
+package com.example.geoquiz
+
+import QuizViewModel
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+import com.example.geoquiz.databinding.ActivityMainBinding
+
+private const val TAG = "MainActivity"
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+    private lateinit var cheatButton: Button
+    private lateinit var nextButton: Button
+    private lateinit var questionTextView: TextView
+    private lateinit var binding: ActivityMainBinding
+    private val quizViewModel: QuizViewModel by viewModels()
+
+    private val cheatLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            quizViewModel.isCheater =
+                result.data?.getBooleanExtra(EXTRA_ANSWER_SHOWN, false) ?: false
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.trueButton.setOnClickListener { view: View ->
+            checkAnswer(true)
+        }
+        binding.falseButton.setOnClickListener { view: View ->
+            checkAnswer(false)
+        }
+        binding.nextButton.setOnClickListener {
+            quizViewModel.moveToNext()
+            updateQuestion()
+        }
+        binding.previousButton.setOnClickListener {
+            quizViewModel.moveToPrevious()
+            updateQuestion()
+        }
+
+        binding.cheatButton.setOnClickListener {
+            // Start CheatActivity
+            val answerIsTrue = quizViewModel.currentQuestionAnswer
+            val intent = CheatActivity.newIntent(this@MainActivity, answerIsTrue)
+            cheatLauncher.launch(intent)
+        }
+
+        updateQuestion()
+    }
+
+    private fun updateQuestion() {
+        val questionTextResId = quizViewModel.currentQuestionText
+        binding.questionTextView.setText(questionTextResId)
+    }
+
+    private fun checkAnswer(userAnswer: Boolean) {
+        val correctAnswer = quizViewModel.currentQuestionAnswer
+
+        val messageResId = when {
+            quizViewModel.isCheater -> R.string.judgment_toast
+            userAnswer == correctAnswer -> R.string.correct_toast
+            else -> R.string.incorrect_toast
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+    }
+
+    // Other lifecycle methods and constant declarations...
+}
